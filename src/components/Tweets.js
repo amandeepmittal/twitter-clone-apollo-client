ececb0ab47726b3605472aca939e39790a399f52
Aman Mittal <amandeepmittal@live.com>
2019-06-02 19:57:44 +0530

    create Tweets component

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,10 +1,23 @@
 import React from "react"
+import "./Tweets.css"
+import TwitterLogo from "../assets/twitter.svg"
 
 class Tweets extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Twitter Clone</h1>
+			<div className='tweets'>
+				<div className='tweet'>
+					<div className='author'>
+						<img src={"https://api.adorable.io/avatars/190/abott@adorable.png"} alt='user-avatar' />
+						<strong>@amanhimself</strong>
+					</div>
+					<div className='content'>
+						<div className='twitter-logo'>
+							<img src={TwitterLogo} alt='twitter-logo' />
+						</div>
+						<textarea autoFocus className='editTextarea' value='' onChange='' />
+					</div>
+				</div>
 			</div>
 		)
 	}
