36cb9b81fa2858aac90d2ff2ad8634033568ad3f
Aman Mittal <amandeepmittal@live.com>
2019-06-02 11:53:36 +0530

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+
+const client = new ApolloClient({
+	uri: "http://localhost:5000/graphiql"
+})
+
+const AppContainer = () => (
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+)
+
+ReactDOM.render(<AppContainer />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
